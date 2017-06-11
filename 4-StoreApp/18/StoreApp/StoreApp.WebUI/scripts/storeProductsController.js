80c26246c63f757a556426a93bc5304c71105167
sadık turan <sadik_turan@hotmail.com>
2017-06-12 02:43:15 +0300

    StoreApp Ders 18-ProductsController

---
+++ 4-StoreApp/18/StoreApp/StoreApp.WebUI/scripts/storeProductsController.js
@@ -0,0 +1,25 @@
+﻿var productUrl = "/api/products/";
+
+var getProducts = function () {
+    sendRequest(productUrl + "getproducts", "Get", null, function (data) {
+        model.products.removeAll();
+        model.products.push.apply(model.products, data);
+    })
+};
+
+var deleteProduct = function (id) {
+    sendRequest(productUrl + "DeleteProduct" + "/" + id, "DELETE", null, function () {
+        model.products.remove(function (item) {
+            return item.Id == id;
+        })
+    })
+};
+
+var saveProduct = function (product, successCallback) {
+    sendRequest(productUrl + "PostProduct", "POST", product, function () {
+        getProducts();
+        if (successCallback) {
+            successCallback();
+        }
+    });
+}
