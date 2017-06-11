b622cfcd584b6824a3eef54f76c2611c95514602
sadık turan <sadik_turan@hotmail.com>
2017-06-12 01:34:21 +0300

    Birthday Ders 6-Jquery ile Kayıt Sorgulama

---
+++ BirthdayApp/06/BirthdayApp/BirthdayApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using BirthdayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DavetiyeFormu()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DavetiyeFormu(DavetiyeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Veritabani.Add(model);
+                return View("Thanks", model);
+            }
+
+            return View(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Katilanlar()
+        {
+            return PartialView(Veritabani.Liste.Where(i => i.KatilmaDurumu == true));
+        }
+
+
+    }
+}
---
+++ BirthdayApp/06/BirthdayApp/BirthdayApp/Models/Veritabani.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayApp.Models
+{
+    public static class Veritabani
+    {
+        private static Dictionary<string, DavetiyeModel> _liste;
+
+        static Veritabani()
+        {
+            _liste = new Dictionary<string, DavetiyeModel>();
+
+            _liste.Add("Hasan", new DavetiyeModel
+            {
+                Ad = "Hasan",
+                Eposta = "hasan@gmail.com",
+                KatilmaDurumu = true
+            });
+
+            _liste.Add("Mehmet", new DavetiyeModel
+            {
+                Ad = "Mehmet",
+                Eposta = "mehmet@gmail.com",
+                KatilmaDurumu = false
+            });
+
+            _liste.Add("Berrin", new DavetiyeModel
+            {
+                Ad = "Berrin",
+                Eposta = "berrin@gmail.com",
+                KatilmaDurumu = true
+            });
+
+        }
+
+        public static void Add(DavetiyeModel model)
+        {
+            string key = model.Ad.ToLower();
+            if (_liste.ContainsKey(key))
+            {
+                _liste[key] = model;
+            }
+            else
+            {
+                _liste.Add(key, model);
+            }
+        }
+
+        public static IEnumerable<DavetiyeModel> Liste
+        {
+            get { return _liste.Values; }
+        }
+    }
+}
