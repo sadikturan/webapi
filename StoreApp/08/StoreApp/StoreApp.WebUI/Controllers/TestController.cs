9b4de62a6006a821a520296190fdd2a597d7d3d5
sadık turan <sadik_turan@hotmail.com>
2017-06-12 00:39:17 +0300

    Ders 8- Data Initializer

---
+++ StoreApp/08/StoreApp/StoreApp.WebUI/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using StoreApp.Data.Abstract;
+using StoreApp.Data.Concrete.EntityFramework;
+using StoreApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreApp.WebUI.Controllers
+{
+    public class TestController : Controller
+    {
+        IProductRepository _productRepository;
+        ICategoryRepository _categoryRepository;
+        IOrderRepository _orderRepository;
+        IOrderLineRepository _orderlineRepository;
+
+        public TestController()
+        {
+            _productRepository = new EfProductRepository();
+            _categoryRepository = new EfCategoryRepository();
+            _orderRepository = new EfOrderRepository();
+            _orderlineRepository = new EfOrderLineRepository();
+        }
+
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View(_productRepository.Products);
+        }
+
+        public async Task<ActionResult> DeleteProduct(int Id)
+        {
+            await _productRepository.DeleteProductAsync(Id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> SaveProduct(Product product)
+        {
+            await _productRepository.SaveProductAsync(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
