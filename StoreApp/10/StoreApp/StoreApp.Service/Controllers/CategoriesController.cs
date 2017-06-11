3b78b9f2bb5262a62535fbf008e101b12e3dfecc
sadık turan <sadik_turan@hotmail.com>
2017-06-12 00:42:57 +0300

    Ders 10-ApiController

---
+++ StoreApp/10/StoreApp/StoreApp.Service/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using StoreApp.Data.Abstract;
+using StoreApp.Data.Concrete.EntityFramework;
+using StoreApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace StoreApp.Service.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        ICategoryRepository _categoryRepository;
+
+        public CategoriesController()
+        {
+            _categoryRepository = new EfCategoryRepository();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _categoryRepository.Categories;
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _categoryRepository.Categories.Where(i => i.Id == id).FirstOrDefault();
+        }
+
+        public async Task PostCategory(Category entity)
+        {
+            await _categoryRepository.SaveCategoryAsync(entity);
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            await _categoryRepository.DeleteCategoryAsync(id);
+        }
+    }
+}
---
+++ StoreApp/10/StoreApp/StoreApp.Service/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using StoreApp.Data.Abstract;
+using StoreApp.Data.Concrete.EntityFramework;
+using StoreApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace StoreApp.Service.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        IProductRepository _productRepository;
+
+        public ProductsController()
+        {
+            //dependecy injection
+            _productRepository = new EfProductRepository();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _productRepository.Products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _productRepository.Products.Where(i => i.Id == id).FirstOrDefault();
+        }
+
+        public async Task PostProduct(Product entity)
+        {
+            await _productRepository.SaveProductAsync(entity);
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            await _productRepository.DeleteProductAsync(id);
+        }
+    }
+}
