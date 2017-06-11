9f421f24f85156593b20f6a5c4b3dd05acee0899
sadık turan <sadik_turan@hotmail.com>
2017-06-12 01:36:34 +0300

    Birthday Ders 10-KnockoutJS Metotları

---
+++ BirthdayApp/10/BirthdayApp/BirthdayApp/Scripts/BirthdayApp.js
@@ -0,0 +1,49 @@
+﻿var model =
+{
+    view: ko.observable("hosgeldin"),
+    davetiyeModel: {
+        Ad: ko.observable(""),
+        Eposta: ko.observable(""),
+        katilmaDurumu: ko.observable("true")
+    },
+    Katilanlar: ko.observableArray([])
+}
+
+var formuGoster = function () {
+    model.view("form");
+}
+
+var formuGonder = function () {
+    $.ajax("/api/Davetiye/Ekle",
+        {
+            type: "POST",
+            data: {
+                Ad: model.davetiyeModel.Ad,
+                Eposta: model.davetiyeModel.Eposta,
+                KatilmaDurumu: model.davetiyeModel.katilmaDurumu
+            },
+            success: function () {
+                katilanlariGetir();
+            }
+
+        });
+}
+
+var katilanlariGetir = function () {
+
+    $.ajax("/api/Davetiye/GetKatilanlar", {
+        type: "GET",
+        success: function (data) {
+            model.Katilanlar.removeAll();
+            model.Katilanlar.push.apply(model.Katilanlar, data.map(function (item) {
+                return item.Ad;
+            }));
+            model.view("Thanks");
+        }
+    });
+
+}
+
+$(document).ready(function () {
+    ko.applyBindings();
+});
