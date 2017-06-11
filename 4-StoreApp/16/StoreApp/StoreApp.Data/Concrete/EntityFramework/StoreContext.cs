d318445579e0e9678f9850b44179069aaa10b2a3
sadık turan <sadik_turan@hotmail.com>
2017-06-12 02:37:47 +0300

    StoreApp Ders 16-Ajax Javascript Kütüphanesi

---
+++ 4-StoreApp/16/StoreApp/StoreApp.Data/Concrete/EntityFramework/StoreContext.cs
@@ -0,0 +1,23 @@
+﻿using StoreApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Data.Concrete.EntityFramework
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext() : base("storeConnection")
+        {
+            Database.SetInitializer(new StoreDataInitializer());
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+    }
+}
