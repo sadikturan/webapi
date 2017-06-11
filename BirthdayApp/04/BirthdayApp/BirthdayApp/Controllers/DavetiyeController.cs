47c20fcae0ce0b7d746640fa906c60e460eb79dd
sadık turan <sadik_turan@hotmail.com>
2017-06-12 01:28:14 +0300

    BirthdayApp Ders 4 - Postman ile WebApi Testi

---
+++ BirthdayApp/04/BirthdayApp/BirthdayApp/Controllers/DavetiyeController.cs
@@ -0,0 +1,32 @@
+﻿using BirthdayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BirthdayApp.Controllers
+{
+    public class DavetiyeController : ApiController
+    {
+       
+        public IEnumerable<DavetiyeModel> GetKatilanlar()
+        {
+            return Veritabani.Liste.Where(i => i.KatilmaDurumu == true);
+        }
+
+        public IEnumerable<DavetiyeModel> GetKatilmayanlar()
+        {
+            return Veritabani.Liste.Where(i => i.KatilmaDurumu == false);
+        }
+
+        public void Ekle(DavetiyeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Veritabani.Add(model);
+            }
+        }
+    }
+}
