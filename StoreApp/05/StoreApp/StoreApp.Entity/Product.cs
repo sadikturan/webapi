4c57f992753f7690ded698db70936ffb642586f6
sadık turan <sadik_turan@hotmail.com>
2017-06-12 00:21:00 +0300

    Ders 5-Abstract Repository

---
+++ StoreApp/05/StoreApp/StoreApp.Entity/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Entity
+{
+ public   class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
